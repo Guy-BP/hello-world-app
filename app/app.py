928a59eedbfe4acdc4e2ada5bcbbd39cae04d98f
Guy Ben Porath <guy66bp@gmail.com>
2025-12-21 13:12:53 +0200

    Change greeting message in Flask app

--- app/app.py
+++ app/app.py
@@ -2,11 +2,9 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-SECRET = os.getenv("APP_SECRET", "not set")
-
 @app.route('/')
 def hello():
-    return f"Hello, World! Secret: {SECRET}"
+    return f"Hello Agora, from Guy Ben Porath :)"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
